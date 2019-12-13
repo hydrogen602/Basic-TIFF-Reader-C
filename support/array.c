6f24c8b9d9c1adcbf9104923711e45147367e257
Jonathan Rotter <hydrogen31415@gmail.com>
2019-12-13 14:28:36 -0600

    buffer

--- support/array.c
+++ support/array.c
@@ -24,7 +24,11 @@ void* newArray(size_t length, size_t size) {
 
 void* __newExpandableArray(size_t length, size_t size, size_t maxLen) {
     // [expandingArrayData_t][arrayData_t] array
-    expandingArrayData_t* ptr = malloc(maxLen * size + sizeof(arrayData_t) + sizeof(expandingArrayData_t));
+    unsigned char* tmp = malloc(maxLen * size + sizeof(arrayData_t) + sizeof(expandingArrayData_t));
+    for (size_t t = 0; t < maxLen * size + sizeof(arrayData_t) + sizeof(expandingArrayData_t); ++t) {
+        tmp[t] = 0;
+    }
+    expandingArrayData_t* ptr = (expandingArrayData_t*)tmp;
     ptr->elementSize = size;
     ptr->maxLen = maxLen;
     arrayData_t* ptr_tmp = (arrayData_t*) (ptr + 1);
@@ -39,18 +43,50 @@ void* __newExpandableArray(size_t length, size_t size, size_t maxLen) {
 }
 
 void* newExpandableArray(size_t length, size_t size) {
-    return __newExpandableArray(length, size, length);
+    size_t sz = (length < 16) ? 16 : length;
+    return __newExpandableArray(length, size, sz);
+}
+
+byte* __newBuffer(size_t length, size_t maxLen) {
+    byte* ptr = __newExpandableArray(length, 1, maxLen);
+
+    arrayData_t* p = ptr;
+    p--;
+    p->identifier = BUFFER_ARRAY;
+
+    return ptr;
+}
+
+byte* newBuffer(size_t length) {
+    size_t sz = (length < 16) ? 16 : length;
+    if (sizeof(byte) != 1) {
+        printErrMsg("unsigned char needs to be 1 byte long for this code");
+        exit(EXIT_FAILURE);
+    }
+
+    return __newBuffer(length, sz);
 }
 
 size_t len(void* ptr) {
     arrayData_t* p = ptr;
     p--;
-    if (p->identifier != EXPANDING_ARRAY && p->identifier != FIXED_ARRAY) {
+    if (p->identifier != EXPANDING_ARRAY && p->identifier != FIXED_ARRAY && p->identifier != BUFFER_ARRAY) {
         printErrMsg("Array invalid");
         fprintf(stderr, "identifier is %x\n", p->identifier);
         return 0;
     }
     return p->len;
+}
+
+unsigned char arrayType(void* ptr) {
+    arrayData_t* p = ptr;
+    p--;
+    if (p->identifier != EXPANDING_ARRAY && p->identifier != FIXED_ARRAY && p->identifier != BUFFER_ARRAY) {
+        printErrMsg("Array invalid");
+        fprintf(stderr, "identifier is %x\n", p->identifier);
+        return 0;
+    }
+    return p->identifier;
 }
 
 // checks if the index requires expanding the array
@@ -83,6 +119,54 @@ void* checkIndex(void* ptr, size_t index) {
     else if (p->identifier == FIXED_ARRAY) {
         printErrMsg("This is a fixed array, checkIndex should only be called on expanding arrays");
     }
+    else if (p->identifier == BUFFER_ARRAY) {
+        printErrMsg("This is a buffer, checkIndex should only be called on expanding arrays");
+    }
+    else {
+        printErrMsg("Array Invalid");
+    }
+
+    return ptr;
+}
+
+byte* checkBufferIndex(byte* ptr, size_t offset, size_t elementSize) {
+    arrayData_t* p = ptr;
+    p--;
+    if (p->identifier == BUFFER_ARRAY) {
+        if (offset + elementSize < len(ptr)) {
+            // nothing to do
+        }
+        else {
+            // expand array!
+            expandingArrayData_t* ead = (expandingArrayData_t*) p;
+            ead--;
+            if (offset + elementSize < ead->maxLen) {
+                // enough space still there
+                p->len += elementSize;
+            }
+            else {
+                // expand memory block
+                size_t newSize = ead->maxLen;
+                do {
+                    newSize *= 2;
+                } while(newSize <= offset + elementSize);
+                
+                printf("Expanding to %lu\n", newSize);
+                printf("offset = %ld\n", offset);
+                void* ptrNew = __newExpandableArray(p->len + 1, ead->elementSize, ead->maxLen * 2);
+                // change ptr
+                memcpy(ptrNew, ptr, p->len * ead->elementSize);
+                freeArray(ptr);
+                return ptrNew;
+            }
+        }
+    }
+    else if (p->identifier == FIXED_ARRAY) {
+        printErrMsg("This is a fixed array, checkBufferIndex should only be called on buffers");
+    }
+    else if (p->identifier == EXPANDING_ARRAY) {
+        printErrMsg("This is an expanding array, checkBufferIndex should only be called on buffers");
+    }
     else {
         printErrMsg("Array Invalid");
     }
@@ -95,7 +179,7 @@ void freeArray(void* ptr) {
     if ((p - 1)->identifier == FIXED_ARRAY) {
         free(p - 1);
     }
-    else if ((p - 1)->identifier == EXPANDING_ARRAY) {
+    else if ((p - 1)->identifier == EXPANDING_ARRAY || (p - 1)->identifier == BUFFER_ARRAY) {
         free((char*)p - sizeof(arrayData_t) - sizeof(expandingArrayData_t));
     }
     else {
@@ -107,6 +191,7 @@ void freeArray(void* ptr) {
  * Test code for fixed array, expanding array
  * Check with valgrind to ensure that there are no memory issues
  */
+#ifdef TESTING
 int main(void) {
     // expandable array
     printf("basic expandable array test\n");
@@ -151,5 +236,22 @@ int main(void) {
     }
 
     freeArray(arr);
+
+    printf("more expanding array testing\n");
+    // append macro
+    arr = newExpandableArray(0, sizeof(int));
+
+    for (int i = 0; i < 30; i++) {
+        append(arr, i * i);
+        //arr = checkIndex(arr, i);
+        //arr[i] = i * i;
+    }
+
+    for (int i = 0; i < len(arr); i++) {
+        printf("arr[%d] = %d\n", i, arr[i]);
+    }
+
+    freeArray(arr);
+
 }
-//*/
+#endif
--- support/array.h
+++ support/array.h
@@ -5,6 +5,9 @@
 
 #define FIXED_ARRAY 0xfa
 #define EXPANDING_ARRAY 0xea
+#define BUFFER_ARRAY 0xba
+
+typedef unsigned char byte;
 
 /*
  * runs checkIndex to expand an expandable array if necessary, then sets the index of the array to the given value
@@ -14,6 +17,7 @@
  * - value is the value the index of the array should be set to
  */
 #define setIndex(arr, index, value) (arr) = checkIndex((arr), (index)); *((arr) + (index)) = (value);
+#define append(arr, value) (arr) = checkIndex((arr), len(arr)); *((arr) + len(arr) - 1) = (value);
 
 typedef struct _BasicArrayData {
     size_t len;
@@ -41,17 +45,35 @@ void* newArray(size_t length, size_t size);
  * 
  * NOTE:
  *  It can be indexed like a regular array, i.e. arr[i] = 5;
- *  Before setting an index, run ptr = checkIndex(ptr, index); on first!
+ *  Before setting an index, run ptr = checkIndex(ptr, index); first!
  *  
  * The setIndex macro combines checkIndex with setting the array index for easier use
  */
 void* newExpandableArray(size_t length, size_t size);
 
 /*
- * return the length of an array
+ * make a new array of variable size and type byte, aka unsigned char
+ * this is useful for reading and writing binary file formats where
+ * the data in those bytes could be of differing sizes and types
+ * 
+ * NOTE:
+ *  It can be indexed like a regular array, i.e. arr[i] = 5;
+ *  Before setting an index, run ptr = checkBufferIndex(ptr, index, elementSize); first!
+ *  
+ */
+byte* newBuffer(size_t length);
+
+/*
+ * returns the length of an array
  * - ptr is either a fixed or variable array
  */
 size_t len(void* ptr);
+
+/*
+ * returns the type of an array
+ * - result is one of the three macros: FIXED_ARRAY, EXPANDING_ARRAY, or BUFFER_ARRAY
+ */
+unsigned char arrayType(void* ptr);
 
 /*
  * check if an index for an expandable array requires expanding the size of the array
@@ -65,6 +87,20 @@ size_t len(void* ptr);
  * The setIndex macro combines checkIndex with setting the array index for easier use
  */
 void* checkIndex(void* ptr, size_t index);
+
+/*
+ * check if an index for a buffer requires expanding the size of the array
+ * should be called before whenever bytes are written to the buffer
+ * - ptr is an expandable array
+ * - offset is where the write to the buffer starts
+ *          - NOTE: offset should be greater or equal to zero
+ * - elementSize is the number of bytes being written
+ *      
+ * usage:
+ *  arr = checkBufferIndex(arr, offset, sizeof(type));
+ * 
+ */
+byte* checkBufferIndex(byte* ptr, size_t offset, size_t elementSize);
 
 /*
  * frees the memory allocated for an array
